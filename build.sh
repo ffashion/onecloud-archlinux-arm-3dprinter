d99b2dec193a276f79161564fc8a76d234eeacf5
fashion <fashion@openbfdev.com>
2026-04-03 13:13:53 +0800

    fix build: dont use tmpfiles instead of chown

--- build.sh
+++ build.sh
@@ -297,6 +297,12 @@ function build_rootfs() {
 
 	(cd patch/rootfs && tar -c --owner=root --group=root .) | (cd "$rootfs" && tar -xp)
 
+	# Use tmpfiles have unsefe check. So chown
+	export MOONRAKER_RUNTIME_HOME=/var/opt/moonraker
+    $chrootdo "chown klipper: ${MOONRAKER_RUNTIME_HOME}/config/klipper.cfg"
+    $chrootdo "chown klipper: ${MOONRAKER_RUNTIME_HOME}/config/moonraker.conf"
+
+
     # systemd services
     $chrootdo "systemctl enable $(cat config/services.conf)"
 
